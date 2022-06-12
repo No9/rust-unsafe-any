e0c79e23f0265597f6ab92b296296ff6f474e174
Anton Whalley <anton@venshare.com>
2022-06-12 12:14:50 +0100

    fix: Move to supported trait object

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,4 @@
 #![deny(missing_docs, warnings)]
-
 //! Traits for unsafe downcasting from trait objects to & or &mut references of
 //! concrete types. These should only be used if you are absolutely certain of the
 //! type of the data in said trait object - there be dragons etc.
@@ -7,7 +6,7 @@
 //! Originally inspired by https://github.com/chris-morgan/anymap
 //! and the implementation of `std::any::Any`.
 
-extern crate traitobject;
+extern crate destructure_traitobject;
 
 use std::any::Any;
 use std::mem;
@@ -17,14 +16,14 @@ use std::mem;
 pub trait UnsafeAny: Any {}
 impl<T: Any> UnsafeAny for T {}
 
-impl UnsafeAny {
+impl dyn UnsafeAny {
     /// Returns a reference to the contained value, assuming that it is of type `T`.
     ///
     /// ## Warning
     ///
     /// If you are not _absolutely certain_ of `T` you should _not_ call this!
     pub unsafe fn downcast_ref_unchecked<T: Any>(&self) -> &T {
-        mem::transmute(traitobject::data(self))
+        mem::transmute(destructure_traitobject::data(self))
     }
 
     /// Returns a mutable reference to the contained value, assuming that it is of type `T`.
@@ -33,7 +32,7 @@ impl UnsafeAny {
     ///
     /// If you are not _absolutely certain_ of `T` you should _not_ call this!
     pub unsafe fn downcast_mut_unchecked<T: Any>(&mut self) -> &mut T {
-        mem::transmute(traitobject::data_mut(self))
+        mem::transmute(destructure_traitobject::data_mut(self))
     }
 
     /// Returns a the contained value, assuming that it is of type `T`.
@@ -41,9 +40,9 @@ impl UnsafeAny {
     /// ## Warning
     ///
     /// If you are not _absolutely certain_ of `T` you should _not_ call this!
-    pub unsafe fn downcast_unchecked<T: Any>(self: Box<UnsafeAny>) -> Box<T> {
-        let raw: *mut UnsafeAny = mem::transmute(self);
-        mem::transmute(traitobject::data_mut(raw))
+    pub unsafe fn downcast_unchecked<T: Any>(self: Box<dyn UnsafeAny>) -> Box<T> {
+        let raw: *mut dyn UnsafeAny = mem::transmute(self);
+        mem::transmute(destructure_traitobject::data_mut(raw))
     }
 }
 
@@ -55,7 +54,7 @@ pub unsafe trait UnsafeAnyExt {
     ///
     /// If you are not _absolutely certain_ of `T` you should _not_ call this!
     unsafe fn downcast_ref_unchecked<T: Any>(&self) -> &T {
-        mem::transmute(traitobject::data(self))
+        mem::transmute(destructure_traitobject::data(self))
     }
 
     /// Returns a mutable reference to the contained value, assuming that it is of type `T`.
@@ -64,7 +63,7 @@ pub unsafe trait UnsafeAnyExt {
     ///
     /// If you are not _absolutely certain_ of `T` you should _not_ call this!
     unsafe fn downcast_mut_unchecked<T: Any>(&mut self) -> &mut T {
-        mem::transmute(traitobject::data_mut(self))
+        mem::transmute(destructure_traitobject::data_mut(self))
     }
 
     /// Returns a the contained value, assuming that it is of type `T`.
@@ -74,52 +73,52 @@ pub unsafe trait UnsafeAnyExt {
     /// If you are not _absolutely certain_ of `T` you should _not_ call this!
     unsafe fn downcast_unchecked<T: Any>(self: Box<Self>) -> Box<T> {
         let raw: *mut Self = mem::transmute(self);
-        mem::transmute(traitobject::data_mut(raw))
+        mem::transmute(destructure_traitobject::data_mut(raw))
     }
 }
 
-unsafe impl UnsafeAnyExt for Any { }
-unsafe impl UnsafeAnyExt for UnsafeAny { }
-unsafe impl UnsafeAnyExt for Any + Send { }
-unsafe impl UnsafeAnyExt for Any + Sync { }
-unsafe impl UnsafeAnyExt for Any + Send + Sync { }
-unsafe impl UnsafeAnyExt for UnsafeAny + Send { }
-unsafe impl UnsafeAnyExt for UnsafeAny + Sync { }
-unsafe impl UnsafeAnyExt for UnsafeAny + Send + Sync { }
+unsafe impl UnsafeAnyExt for dyn Any { }
+unsafe impl UnsafeAnyExt for dyn UnsafeAny { }
+unsafe impl UnsafeAnyExt for dyn Any + Send { }
+unsafe impl UnsafeAnyExt for dyn Any + Sync { }
+unsafe impl UnsafeAnyExt for dyn Any + Send + Sync { }
+unsafe impl UnsafeAnyExt for dyn UnsafeAny + Send { }
+unsafe impl UnsafeAnyExt for dyn UnsafeAny + Sync { }
+unsafe impl UnsafeAnyExt for dyn UnsafeAny + Send + Sync { }
 
 #[cfg(test)]
 mod test {
     use super::{UnsafeAny, UnsafeAnyExt};
     use std::any::Any;
-
+    #[allow(unstable_name_collisions)]
     #[test] fn test_simple_downcast_ext() {
-        let a = Box::new(7usize) as Box<Any>;
+        let a = Box::new(7usize) as Box<dyn Any>;
         unsafe { assert_eq!(*a.downcast_ref_unchecked::<usize>(), 7); }
 
-        let mut a = Box::new(7usize) as Box<Any>;
+        let mut a = Box::new(7usize) as Box<dyn Any>;
         unsafe { assert_eq!(*a.downcast_mut_unchecked::<usize>(), 7); }
 
-        let mut a = Box::new(7usize) as Box<Any>;
+        let mut a = Box::new(7usize) as Box<dyn Any>;
         unsafe {
             *a.downcast_mut_unchecked::<usize>() = 8;
             assert_eq!(*a.downcast_mut_unchecked::<usize>(), 8);
         }
     }
-
+    #[allow(unstable_name_collisions)]
     #[test] fn test_simple_downcast_inherent() {
-        let a = Box::new(7usize) as Box<UnsafeAny>;
+        let a = Box::new(7usize) as Box<dyn UnsafeAny>;
         unsafe { assert_eq!(*a.downcast_ref_unchecked::<usize>(), 7); }
 
-        let mut a = Box::new(7usize) as Box<UnsafeAny>;
+        let mut a = Box::new(7usize) as Box<dyn UnsafeAny>;
         unsafe { assert_eq!(*a.downcast_mut_unchecked::<usize>(), 7); }
 
-        let mut a = Box::new(7usize) as Box<UnsafeAny>;
+        let mut a = Box::new(7usize) as Box<dyn UnsafeAny>;
         unsafe {
             *a.downcast_mut_unchecked::<usize>() = 8;
             assert_eq!(*a.downcast_mut_unchecked::<usize>(), 8);
         }
     }
-
+    #[allow(unstable_name_collisions)]
     #[test] fn test_box_downcast_no_double_free() {
         use std::sync::atomic::{AtomicUsize, Ordering};
         use std::sync::Arc;
@@ -135,7 +134,7 @@ mod test {
         }
 
         let x = Arc::new(AtomicUsize::new(0));
-        let a = Box::new(Dropper { x: x.clone() }) as Box<UnsafeAny>;
+        let a = Box::new(Dropper { x: x.clone() }) as Box<dyn UnsafeAny>;
 
         let dropper = unsafe { a.downcast_unchecked::<Dropper>() };
         drop(dropper);
@@ -144,7 +143,7 @@ mod test {
 
         // Test the UnsafeAnyExt implementation.
         let x = Arc::new(AtomicUsize::new(0));
-        let a = Box::new(Dropper { x: x.clone() }) as Box<Any>;
+        let a = Box::new(Dropper { x: x.clone() }) as Box<dyn Any>;
 
         let dropper = unsafe { a.downcast_unchecked::<Dropper>() };
         drop(dropper);
